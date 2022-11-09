73adb7dc9c8142d681dbbf8ddf1cc056bdc62954
James Wong <james.wong17@protonmail.com>
2022-11-09 13:57:22 +1100

    changed theme color and implemented header buttons

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -3,31 +3,44 @@
  * https://reactnavigation.org/docs/getting-started
  *
  */
-import { FontAwesome } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import * as React from 'react';
-import { ColorSchemeName, Pressable } from 'react-native';
+import { FontAwesome } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as React from "react";
+import { ColorSchemeName, Pressable, View } from "react-native";
 
-import Colors from '../constants/Colors';
-import useColorScheme from '../hooks/useColorScheme';
-import ModalScreen from '../screens/ModalScreen';
-import NotFoundScreen from '../screens/NotFoundScreen';
-import TabOneScreen from '../screens/Home';
-import TabTwoScreen from '../screens/Devices';
-import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
-import LinkingConfiguration from './LinkingConfiguration';
-import Home from '../assets/icons/Home';
-import Devices from '../screens/Devices';
-import Guides from '../screens/Guides';
-import Services from '../screens/Services';
+import Colors from "../constants/Colors";
+import useColorScheme from "../hooks/useColorScheme";
+import ModalScreen from "../screens/ModalScreen";
+import NotFoundScreen from "../screens/NotFoundScreen";
+import TabOneScreen from "../screens/Home";
+import TabTwoScreen from "../screens/Devices";
+import {
+  RootStackParamList,
+  RootTabParamList,
+  RootTabScreenProps,
+} from "../types";
+import LinkingConfiguration from "./LinkingConfiguration";
+import Home from "../assets/icons/Home";
+import Devices from "../screens/Devices";
+import Guides from "../screens/Guides";
+import Services from "../screens/Services";
 
-export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
+export default function Navigation({
+  colorScheme,
+}: {
+  colorScheme: ColorSchemeName;
+}) {
   return (
     <NavigationContainer
       linking={LinkingConfiguration}
-      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+    >
       <RootNavigator />
     </NavigationContainer>
   );
@@ -42,9 +55,17 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 function RootNavigator() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Root" component={BottomTabNavigator} options={{ headerShown: false }} />
-      <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
-      <Stack.Group screenOptions={{ presentation: 'modal' }}>
+      <Stack.Screen
+        name="Root"
+        component={BottomTabNavigator}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="NotFound"
+        component={NotFoundScreen}
+        options={{ title: "Oops!" }}
+      />
+      <Stack.Group screenOptions={{ presentation: "modal" }}>
         <Stack.Screen name="Modal" component={ModalScreen} />
       </Stack.Group>
     </Stack.Navigator>
@@ -65,31 +86,67 @@ function BottomTabNavigator() {
       initialRouteName="TabOne"
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme].tint,
-        tabBarStyle: {height: 90}
-      }}>
+        tabBarStyle: { height: 90 },
+      }}
+    >
       <BottomTab.Screen
         name="TabOne"
         component={TabOneScreen}
-        options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
-          title: 'Dashboard',
+        options={({ navigation }: RootTabScreenProps<"TabOne">) => ({
+          title: "Dashboard",
           tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+          // tabBarIcon: ({ color }) => <Home />,
           headerStyle: {
             height: 110,
           },
           headerRight: () => (
-            <Pressable
-              onPress={() => navigation.navigate('Modal')}
-              style={({ pressed }) => ({
-                opacity: pressed ? 0.5 : 1,
-              })}>
-              <FontAwesome
-                name="bell"
-                size={25}
-                color={Colors[colorScheme].text}
-                style={{ marginRight: 15, backgroundColor: 'D9D9D9', borderRadius: 20, padding: 7, overflow: 'hidden' }}
-              />
-            </Pressable>
-            
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+              }}
+            >
+              <Pressable
+                onPress={() => navigation.navigate("Modal")}
+                style={({ pressed }) => ({
+                  opacity: pressed ? 0.5 : 1,
+                  marginRight: 15,
+                  backgroundColor: "#c6cccc",
+                  borderRadius: 20,
+                  padding: 8,
+                  overflow: "hidden",
+                })}
+              >
+                <FontAwesome
+                  name="bell"
+                  size={22}
+                  color={"white"}
+                  style={{
+                    color: "black",
+                    paddingHorizontal: 1,
+                  }}
+                />
+              </Pressable>
+              <Pressable
+                onPress={() => navigation.navigate("Modal")}
+                style={({ pressed }) => ({
+                  opacity: pressed ? 0.5 : 1,
+                  marginRight: 15,
+                  backgroundColor: "#c6cccc",
+                  borderRadius: 20,
+                  padding: 7,
+                })}
+              >
+                <FontAwesome
+                  name="user"
+                  size={25}
+                  color={Colors[colorScheme].text}
+                  style={{
+                    paddingHorizontal: 4,
+                  }}
+                />
+              </Pressable>
+            </View>
           ),
         })}
       />
@@ -97,49 +154,120 @@ function BottomTabNavigator() {
         name="TabTwo"
         component={Devices}
         options={{
-          title: 'Devices',
+          title: "Devices",
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        
+
           headerStyle: {
             height: 110,
           },
           headerRight: () => (
-            // <Pressable
-            //   onPress={() => navigation.navigate('Modal')}
-            //   style={({ pressed }) => ({
-            //     opacity: pressed ? 0.5 : 1,
-            //   })}>
-              <FontAwesome
-                name="bell"
-                size={25}
-                color={Colors[colorScheme].text}
-                style={{ marginRight: 15, backgroundColor: 'D9D9D9', borderRadius: 20, padding: 7, overflow: 'hidden' }}
-              />
-            // </Pressable>
-          ),}}
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+              }}
+            >
+              <Pressable
+                onPress={() => navigation.navigate("Modal")}
+                style={({ pressed }) => ({
+                  opacity: pressed ? 0.5 : 1,
+                  marginRight: 15,
+                  backgroundColor: "#c6cccc",
+                  borderRadius: 20,
+                  padding: 8,
+                  overflow: "hidden",
+                })}
+              >
+                <FontAwesome
+                  name="bell"
+                  size={22}
+                  color={"white"}
+                  style={{
+                    color: "black",
+                    paddingHorizontal: 1,
+                  }}
+                />
+              </Pressable>
+              <Pressable
+                onPress={() => navigation.navigate("Modal")}
+                style={({ pressed }) => ({
+                  opacity: pressed ? 0.5 : 1,
+                  marginRight: 15,
+                  backgroundColor: "#c6cccc",
+                  borderRadius: 20,
+                  padding: 7,
+                })}
+              >
+                <FontAwesome
+                  name="user"
+                  size={25}
+                  color={Colors[colorScheme].text}
+                  style={{
+                    paddingHorizontal: 4,
+                  }}
+                />
+              </Pressable>
+            </View>
+          ),
+        }}
       />
       <BottomTab.Screen
         name="TabThree"
         component={Guides}
         options={{
-          title: 'Guides',
+          title: "Guides",
           tabBarIcon: ({ color }) => <TabBarIcon name="book" color={color} />,
           headerStyle: {
             height: 110,
           },
           headerRight: () => (
-            // <Pressable
-            //   onPress={() => navigation.navigate('Modal')}
-            //   style={({ pressed }) => ({
-            //     opacity: pressed ? 0.5 : 1,
-            //   })}>
-              <FontAwesome
-                name="bell"
-                size={25}
-                color={Colors[colorScheme].text}
-                style={{ marginRight: 15, backgroundColor: 'D9D9D9', borderRadius: 20, padding: 7, overflow: 'hidden' }}
-              />
-            // </Pressable>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+              }}
+            >
+              <Pressable
+                onPress={() => navigation.navigate("Modal")}
+                style={({ pressed }) => ({
+                  opacity: pressed ? 0.5 : 1,
+                  marginRight: 15,
+                  backgroundColor: "#c6cccc",
+                  borderRadius: 20,
+                  padding: 8,
+                  overflow: "hidden",
+                })}
+              >
+                <FontAwesome
+                  name="bell"
+                  size={22}
+                  color={"white"}
+                  style={{
+                    color: "black",
+                    paddingHorizontal: 1,
+                  }}
+                />
+              </Pressable>
+              <Pressable
+                onPress={() => navigation.navigate("Modal")}
+                style={({ pressed }) => ({
+                  opacity: pressed ? 0.5 : 1,
+                  marginRight: 15,
+                  backgroundColor: "#c6cccc",
+                  borderRadius: 20,
+                  padding: 7,
+                })}
+              >
+                <FontAwesome
+                  name="user"
+                  size={25}
+                  color={Colors[colorScheme].text}
+                  style={{
+                    paddingHorizontal: 4,
+                  }}
+                />
+              </Pressable>
+            </View>
           ),
         }}
       />
@@ -147,24 +275,61 @@ function BottomTabNavigator() {
         name="TabFour"
         component={Services}
         options={{
-          title: 'Services',
-          tabBarIcon: ({ color }) => <TabBarIcon name="recycle" color={color} />,
+          title: "Services",
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="recycle" color={color} />
+          ),
           headerStyle: {
             height: 110,
           },
           headerRight: () => (
-            // <Pressable
-            //   onPress={() => navigation.navigate('Modal')}
-            //   style={({ pressed }) => ({
-            //     opacity: pressed ? 0.5 : 1,
-            //   })}>
-              <FontAwesome
-                name="bell"
-                size={25}
-                color={Colors[colorScheme].text}
-                style={{ marginRight: 15, backgroundColor: 'D9D9D9', borderRadius: 20, padding: 7, overflow: 'hidden' }}
-              />
-            // </Pressable>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+              }}
+            >
+              <Pressable
+                onPress={() => navigation.navigate("Modal")}
+                style={({ pressed }) => ({
+                  opacity: pressed ? 0.5 : 1,
+                  marginRight: 15,
+                  backgroundColor: "#c6cccc",
+                  borderRadius: 20,
+                  padding: 8,
+                  overflow: "hidden",
+                })}
+              >
+                <FontAwesome
+                  name="bell"
+                  size={22}
+                  color={"white"}
+                  style={{
+                    color: "black",
+                    paddingHorizontal: 1,
+                  }}
+                />
+              </Pressable>
+              <Pressable
+                onPress={() => navigation.navigate("Modal")}
+                style={({ pressed }) => ({
+                  opacity: pressed ? 0.5 : 1,
+                  marginRight: 15,
+                  backgroundColor: "#c6cccc",
+                  borderRadius: 20,
+                  padding: 7,
+                })}
+              >
+                <FontAwesome
+                  name="user"
+                  size={25}
+                  color={Colors[colorScheme].text}
+                  style={{
+                    paddingHorizontal: 4,
+                  }}
+                />
+              </Pressable>
+            </View>
           ),
         }}
       />
@@ -176,9 +341,8 @@ function BottomTabNavigator() {
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
+  name: React.ComponentProps<typeof FontAwesome>["name"];
   color: string;
 }) {
   return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
 }
-
