33c3e8c47df28e8be577b639479a7eb743280cda
z5310223 <z5310223@ad.unsw.edu.au>
2022-11-10 17:12:06 +1100

    added profile image and formatted info

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -1,3 +1,4 @@
+import { FontAwesome } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
 import { Platform, StyleSheet, TouchableOpacity } from 'react-native';
 
@@ -14,11 +15,33 @@ function MenuButton({label, colour}) {
   )
 }
 
+function ProfileImage() {
+  return (
+    <View style={styles.circle}>
+      <FontAwesome
+        style={{marginTop: 20}}
+        name="user"
+        size={50}
+      />
+      <FontAwesome
+        style={styles.plus}
+        name="plus-circle"
+        size={25}
+      />
+    </View>
+  )
+}
+
 export default function ModalScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Username</Text>
-      <Text style={{fontSize:16}}>Email</Text>
+      <View style={styles.info}>
+        <ProfileImage/>
+        <View style={styles.infotext}>
+          <Text style={styles.title}>Username</Text>
+          <Text style={{fontSize:16}}>Email</Text>
+        </View>
+      </View>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       {/* <EditScreenInfo path="/screens/ModalScreen.tsx" /> */}
       <MenuButton label={"Redeem Vouchers"} colour={"#708B75"} />
@@ -53,5 +76,30 @@ const styles = StyleSheet.create({
     marginBottom: 25,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  circle: {
+    backgroundColor: 'lightgrey',
+    width: 90,
+    height: 90,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  plus: {
+    marginLeft: 50,
+    backgroundColor: 'white',
+    borderRadius: 50
+  },
+  info: {
+    width: '80%',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    marginRight: 40
+  },
+  infotext: {
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    height: 90
   }
 });
