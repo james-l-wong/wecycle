d13625651162af9061cb513cf1ef202470f6618d
Chris-Luong <64023748+Chris-Luong@users.noreply.github.com>
2022-11-16 18:33:04 +1100

    working on searchbar and guide articles

--- screens/Guides.tsx
+++ screens/Guides.tsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 import { Text } from '../components/Themed';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { View } from '../components/Themed';
 import GuideCard from '../components/GuideCard';
+import SearchBar from '../components/SearchBar';
 
 /**
  * TODO: Multiple guides should be readable and shareable
  * TODO: Once guide card is finalised, send to James
  */
 
+
+
 export default function TabTwoScreen() {
+  const [searchPhrase, setSearchPhrase] = useState(null);
+
+  const search = (input) => {
+    console.log(input);
+  }
+
+  useEffect(() => {
+    search(searchPhrase)
+  }, searchPhrase)
+
   return (
     <ScrollView contentContainerStyle={[styles.container]}>
       <Text style={styles.title}>Guides</Text>
-      <View style={[styles.separator]} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      <SearchBar setText={newText => setSearchPhrase(newText)}/>
+      <View style={[styles.separator]} lightColor="#fff" darkColor="rgba(255,255,255,0.1)" />
+      {/* <EditScreenInfo path="/screens/TabTwoScreen.tsx" /> */}
       <GuideCard />
       <GuideCard />
       <GuideCard />
