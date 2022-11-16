860b7611fd66e9ee3bc6a7ba18236e945c3fdd15
Chris-Luong <64023748+Chris-Luong@users.noreply.github.com>
2022-11-16 13:53:55 +1100

    guide cards displaying nicely with margins

--- components/GuideCard.tsx
+++ components/GuideCard.tsx
@@ -13,159 +13,168 @@ import {
   Pressable,
   Badge,
   Spacer,
-  Text,
   Flex,
 } from 'native-base';
 
+/**
+ * Each guide will have a title, body, summary, thumbnail, author and date
+ * Cards will display title, summary, thumnmail, author and date
+ * Articles have everything except summary as well as a share button
+ * TODO guide article screen is given a blog id (could be title or an id)
+ * If/else statements are run to see which blog to display
+ * blog id is given by guide card, onClick => display article given id
+ * 
+ * Cards and articles should be passed these props or just make them
+ * static elements? Since there is no backend/way or need to add in props?
+ */
 
 const GuideCard = () => {
     return (<Pressable>
         {({
         isHovered,
+        isFocused,
         isPressed
       }) => {
-        return (
-          <Box alignItems="center">
-            <Box
-              maxW="80"
-              rounded="lg"
-              overflow="hidden"
-              borderColor="coolGray.200"
-              borderWidth="1"
-              _dark={{
-                borderColor: "coolGray.600",
-                backgroundColor: "gray.700"
-              }}
-              _web={{
-                shadow: 2,
-                borderWidth: 0
-              }}
-              bg={isPressed
-                ? 'coolGray.200'
-                : isHovered
+        return ( <Box alignItems="center">
+          <Box
+            maxW="80"
+            rounded="lg"
+            overflow="hidden"
+            borderColor="coolGray.200"
+            borderWidth="1"
+            _dark={{
+              borderColor: "coolGray.600",
+              backgroundColor: "gray.700"
+            }}
+            _web={{
+              shadow: 2,
+              borderWidth: 0
+            }}
+            bg={isPressed
+              ? 'coolGray.200'
+              : isHovered
                 ? 'coolGray.200'
                 : 'coolGray.100'}
-              _light={{
-                backgroundColor: "gray.50"
-              }}
-            >
+            _light={{
+              backgroundColor: "gray.50"
+            }}
+            style={[
+              styles.card,
+              {transform: [{
+                scale: isFocused ? 0.96 : 1
+              }]}
+            ]}
+          >
+            <Box>
+              <AspectRatio w="100%" ratio={16 / 9}>
+                <Image
+                  source={{
+                    uri: "https://www.holidify.com/images/cmsuploads/compressed/Bangalore_citycover_20190613234056.jpg"
+                  }} alt="blog-card-image" />
+              </AspectRatio>
+            </Box>
+            <Stack p="4" space={3}>
+              <Stack space={2}>
+                <Heading size="md" ml="-1">
+                  The Garden City
+                </Heading>
+                <NativeBaseText
+                  fontSize="xs"
+                  _light={{
+                    color: "violet.500"
+                  }} _dark={{
+                    color: "violet.400"
+                  }}
+                  fontWeight="500"
+                  ml="-0.5"
+                  mt="-1"
+                >
+                  The Silicon Valley of India.
+                </NativeBaseText>
+              </Stack>
+              <NativeBaseText fontWeight="400">
+                Bengaluru (also called Bangalore) is the center of India's high-tech
+                industry. The city is also known for its parks and nightlife.
+              </NativeBaseText>
+              <HStack alignItems="center" space={4} justifyContent="space-between">
+                <HStack alignItems="center" fontSize={10}>
+                  <NativeBaseText
+                    color="coolGray.600"
+                    _dark={{
+                      color: "warmGray.200"
+                    }}
+                    fontWeight="400"
+                  >
+                    6 mins ago
+                  </NativeBaseText>
+                  <Spacer />
+                  <NativeBaseText color="coolGray.800" _dark={{color: "warmGray.200"}}>
+                    Author name
+                  </NativeBaseText>
+                </HStack>
+              </HStack>
+            </Stack>
+          </Box>
+        </Box>)
+          {/* <Box
+            maxW="96"
+            borderWidth="1"
+            borderColor="coolGray.300"
+            shadow="3"
+            bg={isPressed
+              ? 'coolGray.200'
+              : isHovered
+                ? 'coolGray.200'
+                : 'coolGray.100'}
+            p="5"
+            rounded="8"
+            style={{
+              transform: [{
+                scale: isPressed ? 0.96 : 1
+              }]
+            }}
+          >
               <Box>
                 <AspectRatio w="100%" ratio={16 / 9}>
                   <Image
                     source={{
                       uri: "https://www.holidify.com/images/cmsuploads/compressed/Bangalore_citycover_20190613234056.jpg"
-                    }} alt="blog-card-image"
-                  />
+                    }} alt="blog-card-image" />
                 </AspectRatio>
-                <Center
-                  bg="violet.500"
-                  _dark={{
-                    bg: "violet.400"
-                  }} _text={{
-                    color: "warmGray.50",
-                    fontWeight: "700",
-                    fontSize: "xs"
-                  }}
-                  position="absolute"
-                  bottom="0"
-                  px="3"
-                  py="1.5"
-                >
-                  PHOTOS
-                </Center>
               </Box>
-              <Stack p="4" space={3}>
-                <Stack space={2}>
-                  <Heading size="md" ml="-1">
-                    The Garden City
-                  </Heading>
-                  <NativeBaseText
-                    fontSize="xs"
-                    _light={{
-                      color: "violet.500"
-                    }} _dark={{
-                      color: "violet.400"
-                    }}
-                    fontWeight="500"
-                    ml="-0.5"
-                    mt="-1"
-                  >
-                    The Silicon Valley of India.
-                  </NativeBaseText>
-                </Stack>
-                <NativeBaseText fontWeight="400">
-                  Bengaluru (also called Bangalore) is the center of India's high-tech
-                  industry. The city is also known for its parks and nightlife.
+              <HStack alignItems="center">
+                <Badge
+                  colorScheme="darkBlue"
+                  _text={{
+                    color: 'white'
+                  }}
+                  variant="solid"
+                  rounded="4"
+                >
+                  Business
+                </Badge>
+                <Spacer />
+                <NativeBaseText fontSize={10} color="coolGray.800">
+                  1 month ago
                 </NativeBaseText>
-                <HStack alignItems="center" space={4} justifyContent="space-between">
-                  <HStack alignItems="center">
-                    <NativeBaseText
-                      color="coolGray.600"
-                      _dark={{
-                        color: "warmGray.200"
-                      }}
-                      fontWeight="400"
-                    >
-                      6 mins ago
-                    </NativeBaseText>
-                  </HStack>
-                </HStack>
-              </Stack>
-            </Box>
-          </Box>
-          // <Box
-          //   maxW="96"
-          //   borderWidth="1"
-          //   borderColor="coolGray.300"
-          //   shadow="3"
-          //   bg={isPressed
-          //     ? 'coolGray.200'
-          //     : isHovered
-          //     ? 'coolGray.200'
-          //     : 'coolGray.100'}
-          //   p="5"
-          //   rounded="8"
-          //   style={{
-          //     transform: [{
-          //       scale: isPressed ? 0.96 : 1
-          //     }]
-          //   }}
-          // >
-          //   <HStack alignItems="center">
-          //     <Badge
-          //       colorScheme="darkBlue"
-          //       _text={{
-          //         color: 'white'
-          //       }}
-          //       variant="solid"
-          //       rounded="4"
-          //     >
-          //       Business
-          //     </Badge>
-          //     <Spacer />
-          //     <Text fontSize={10} color="coolGray.800">
-          //       1 month ago
-          //     </Text>
-          //   </HStack>
-          //   <Text color="coolGray.800" mt="3" fontWeight="medium" fontSize="xl">
-          //     Marketing License
-          //   </Text>
-          //   <Text mt="2" fontSize="sm" color="coolGray.700">
-          //     Unlock powerfull time-saving tools for creating email delivery and
-          //     collecting marketing data
-          //   </Text>
-          //   <Flex>
-          //     {isFocused ? <Text mt="2" fontSize={12} fontWeight="medium" textDecorationLine="underline" color="darkBlue.600" alignSelf="flex-start">
-          //         Read More
-          //       </Text> : <Text mt="2" fontSize={12} fontWeight="medium" color="darkBlue.600">
-          //         Read More
-          //       </Text>}
-          //   </Flex>
-          // </Box>
-        )
+              </HStack>
+              <NativeBaseText color="coolGray.800" mt="3" fontWeight="medium" fontSize="xl">
+                Marketing License
+              </NativeBaseText>
+              <NativeBaseText mt="2" fontSize="sm" color="coolGray.700">
+                Unlock powerfull time-saving tools for creating email delivery and
+                collecting marketing data
+              </NativeBaseText>
+              <Flex>
+                {isFocused ? <NativeBaseText mt="2" fontSize={12} fontWeight="medium" textDecorationLine="underline" color="darkBlue.600" alignSelf="flex-start">
+                  Read More
+                </NativeBaseText> : <NativeBaseText mt="2" fontSize={12} fontWeight="medium" color="darkBlue.600">
+                  Read More
+                </NativeBaseText>}
+              </Flex>
+            </Box> */}
       }}
     </Pressable>
-
     );
   };
 export default GuideCard;
@@ -185,7 +194,8 @@ const styles = StyleSheet.create({
       height: 1,
       width: '80%',
     },
-    bottomMargin: {
-      marginBottom: 400,
+    card: {
+      marginTop: 10,
+      marginBottom: 10,
     },
   });
