a82ed9ef5227dc530fbba2c8fa9ca83926104f9d
Hk275 <harshwardhankothari2@gmail.com>
2022-11-17 17:26:56 +1100

    Getting Main:

--- App.tsx
+++ App.tsx
@@ -20,6 +20,7 @@ export default function App() {
           <Navigation colorScheme={colorScheme} />
           <StatusBar />
         </SafeAreaProvider>
+        
       </NativeBaseProvider>
     );
   }
