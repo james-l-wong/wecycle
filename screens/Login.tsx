2c8934f51bf4135b026b223e8394c4b2cf1d035c
James Wong <james.wong17@gmail.com>
2022-11-18 16:03:50 +1100

    fixed rego and login

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -3,9 +3,12 @@ import { Pressable, StyleSheet } from "react-native";
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 import Logo from "../assets/images/logo";
-import { Button, Input } from "native-base";
+import { Button, Icon, Input } from "native-base";
+import React from "react";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function TabTwoScreen({ navigation }) {
+  const [show, setShow] = React.useState(false);
   return (
     <View style={styles.container}>
       <Logo style={{ marginTop: "50%" }} />
@@ -13,25 +16,50 @@ export default function TabTwoScreen({ navigation }) {
         style={{ marginTop: 40 }}
         w="70%"
         variant="underlined"
-        placeholder="name"
+        placeholder="Name"
       />
-      <Input
-        style={{ marginTop: 20 }}
-        w="70%"
-        variant="underlined"
-        placeholder="password"
-      />
+      <View style={{ marginTop: 20 }}>
+        <Input
+          w={{
+            base: "70%",
+            md: "25%",
+          }}
+          variant="underlined"
+          type={show ? "text" : "password"}
+          InputRightElement={
+            <Pressable onPress={() => setShow(!show)}>
+              <Icon
+                as={
+                  <MaterialIcons
+                    name={show ? "visibility" : "visibility-off"}
+                  />
+                }
+                size={5}
+                mr="2"
+                color="muted.400"
+              />
+            </Pressable>
+          }
+          placeholder="Password"
+        />
+      </View>
       <Button
         style={{ marginTop: 40, backgroundColor: "#758A77", width: 275 }}
         onPress={() => navigation.navigate("Home")}
       >
         Login
       </Button>
-      <View style={{flexDirection: "row", justifyContent: "center", paddingTop: 20}}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          paddingTop: 20,
+        }}
+      >
         <Text>Don't have an account? </Text>
         <Text
-          style={{color: "#758A77", textDecorationLine: "underline"}}
-          onPress={()=>navigation.navigate("Register")}
+          style={{ color: "#758A77", textDecorationLine: "underline" }}
+          onPress={() => navigation.navigate("Register")}
         >
           Register
         </Text>
--- screens/Register.tsx
+++ screens/Register.tsx
@@ -3,9 +3,12 @@ import { Pressable, StyleSheet } from "react-native";
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 import Logo from "../assets/images/logo";
-import { Button, Input } from "native-base";
+import { Button, Icon, Input } from "native-base";
+import React from "react";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function TabTwoScreen({ navigation }) {
+  const [show, setShow] = React.useState(false);
   return (
     <View style={styles.container}>
       <Logo style={{ marginTop: "40%" }} />
@@ -13,37 +16,81 @@ export default function TabTwoScreen({ navigation }) {
         style={{ marginTop: 40 }}
         w="70%"
         variant="underlined"
-        placeholder="name"
+        placeholder="Name"
       />
       <Input
         style={{ marginTop: 20 }}
         w="70%"
         variant="underlined"
-        placeholder="email"
+        placeholder="Email"
       />
-      <Input
-        style={{ marginTop: 20 }}
-        w="70%"
-        variant="underlined"
-        placeholder="password"
-      />
-      <Input
-        style={{ marginTop: 20 }}
-        w="70%"
-        variant="underlined"
-        placeholder="confirm password"
-      />
+      <View style={{ marginTop: 20 }}>
+        <Input
+          w={{
+            base: "70%",
+            md: "25%",
+          }}
+          variant="underlined"
+          type={show ? "text" : "password"}
+          InputRightElement={
+            <Pressable onPress={() => setShow(!show)}>
+              <Icon
+                as={
+                  <MaterialIcons
+                    name={show ? "visibility" : "visibility-off"}
+                  />
+                }
+                size={5}
+                mr="2"
+                color="muted.400"
+              />
+            </Pressable>
+          }
+          placeholder="Password"
+        />
+      </View>
+      <View style={{ marginTop: 20 }}>
+        <Input
+          w={{
+            base: "70%",
+            md: "25%",
+          }}
+          variant="underlined"
+          type={show ? "text" : "password"}
+          InputRightElement={
+            <Pressable onPress={() => setShow(!show)}>
+              <Icon
+                as={
+                  <MaterialIcons
+                    name={show ? "visibility" : "visibility-off"}
+                  />
+                }
+                size={5}
+                mr="2"
+                color="muted.400"
+              />
+            </Pressable>
+          }
+          placeholder="Confirm Password"
+        />
+      </View>
       <Button
         style={{ marginTop: 40, backgroundColor: "#758A77", width: 275 }}
         onPress={() => navigation.navigate("Home")}
       >
         Register
       </Button>
-      <View style={{flexDirection: "row", justifyContent: "center", paddingTop: 20}}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          paddingTop: 20,
+        }}
+      >
         <Text>Have an account? </Text>
         <Text
-          style={{color: "#758A77", textDecorationLine: "underline"}}
-          onPress={()=>navigation.navigate("Login")}
+          style={{ color: "#758A77", textDecorationLine: "underline" }}
+          onPress={() => navigation.navigate("Login")}
         >
           Login
         </Text>
